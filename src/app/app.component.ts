fdfb33a484083014182fcbe35c37ea4161015272
unknown <arielc.nunes@outlook.com>
2024-03-07 15:44:53 +0000

    lab05 - read from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentService } from './Services/student.service';
+import { WeatherService } from './Services/weather.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'json-lab5';
+export class AppComponent implements OnInit {
+  students:any = [];
+  weather:any = [];
+  temp:any = "";
+  constructor(private studentService:StudentService, private weatherService:WeatherService) { }
+
+  ngOnInit(): void {
+    this.studentService.GetStudentData().subscribe(
+      (data) => {
+        this.students = data.students; // local array is assiged data.students value from api
+      }
+    );
+    
+    this.weatherService.GetWeatherData().subscribe(
+      (data) => {
+        this.weather = data.weather;
+        this.temp = data.main; // assing temp in data.main to our temp variable
+      }
+    );
+  }
 }
